6974704d93b23835188f9d1ec28739b5fb783fd1
Thomas Witzel <thomas@q.bio>
2023-06-23 12:47:47 -0700

    small change to the read-handshake test

--- src/main.rs
+++ src/main.rs
@@ -86,6 +86,7 @@ fn test_axi4lite_slave_controller_read() {
             sim_assert_eq!(ep, x.axi_bus.RDATA.val(), 0x1234_5678, x);
             sim_assert!(ep, x.axi_bus.RVALID.val(), x);
             x.axi_bus.ARVALID.next = false;
+            x.axi_bus.ARADDR.next = 0x0000_0000.into();
             x.axi_bus.RREADY.next = true;
             wait_clock_cycle!(ep, axi_bus.ACLK, x);
             x.axi_bus.RREADY.next = false;
